e89bc7bb14b2bcaef4eab41361e30d3ca2c56799
Jmuse <60051610+RbxCheats@users.noreply.github.com>
2021-01-01 15:58:31 -0600

    Delete players.php

--- players.php
+++
@@ -1,8 +0,0 @@
-<?php
-$amount = 0
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-  if($_POST['user'] == 1){$amount = $amount + 1; return echo $amount; exit();}
-}
-if($_SERVER['REQUEST_METHOD'] === "GET"){return echo $amount; exit();}
-?>
