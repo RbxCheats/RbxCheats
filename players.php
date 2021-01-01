a77d3be3e2762dd316b7ae9b247a0f1e8f5b5b28
Jmuse <60051610+RbxCheats@users.noreply.github.com>
2021-01-01 16:44:31 -0600

    Update players.php

--- players.php
+++ players.php
@@ -1,4 +1,6 @@
 <?php
+$amount = 0
+
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   if($_POST['user'] === 1){$amount = $amount + 1;echo $amount; exit(); return;}
   if($_POST['getusers'] === 1){echo $amount; exit(); return;}
