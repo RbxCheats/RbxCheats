bc7b8afe4af3040eccbf617768e3638d0f372173
Jmuse <60051610+RbxCheats@users.noreply.github.com>
2026-04-17 19:24:37 -0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('copyButton').addEventListener('click', function() {
-    const textToCopy = document.getElementById('textToCopy');
-    
-    textToCopy.select();
-    document.execCommand('copy');
-});
